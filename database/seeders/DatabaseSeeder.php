20b26ad8eaa7c8296335286adbbf5fd32b9a3af8
Chéridanh <divintsiela@gmail.com>
2025-03-17 23:38:12 +0100

    finish with Database Seeder and review all models and migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,17 +16,15 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Création des rôles
         $admin = Role::factory()->create(['wording' => 'admin']);
         $prof = Role::factory()->create(['wording' => 'professor']);
         $student = Role::factory()->create(['wording' => 'student']);
 
-        // Création des salles de classe
         $btsSioSisr = Classroom::factory()->create(['name' => 'BTS SIO SISR']);
         $btsSioSlam = Classroom::factory()->create(['name' => 'BTS SIO SLAM']);
         $bachelorCda = Classroom::factory()->create(['name' => 'Bachelor CDA']);
+        $classrooms = [$btsSioSisr, $btsSioSlam, $bachelorCda];
 
-        // Création d'un administrateur
         User::factory()
             ->for($admin)
             ->create([
@@ -37,7 +35,6 @@ class DatabaseSeeder extends Seeder
                 'password' => Hash::make('administrator')
             ]);
 
-        // Création d'un professeur et association à deux salles de classe
         $professor = User::factory()
             ->for($prof)
             ->hasAttached([$btsSioSlam, $bachelorCda])
@@ -49,11 +46,9 @@ class DatabaseSeeder extends Seeder
                 'password' => Hash::make('professor')
             ]);
 
-        // Création d'étudiants et association à une salle de classe aléatoire
-        $classrooms = [$btsSioSisr, $btsSioSlam, $bachelorCda];
         User::factory()
             ->for($student)
-            ->count(10)
+            ->count(5)
             ->create()
             ->each(function ($user) use ($classrooms) {
                 $user->classrooms()->attach(
@@ -61,23 +56,22 @@ class DatabaseSeeder extends Seeder
                 );
             });
 
-        // Création d'une évaluation par le professeur pour la salle de classe "BTS SIO SLAM"
         $evaluation = Evaluation::factory()
             ->create([
-                'title' => 'Examen de programmation',
-                'description' => 'Évaluation des compétences en PHP et Laravel',
+                'title' => 'Make MCD',
+                'description' => 'Make MCD for your project of Bachelor !',
                 'min_value' => 0,
                 'max_value' => 20,
             ]);
 
-        // Récupère les étudiants de la salle de classe "BTS SIO SLAM"
+        $evaluation->users()->attach($professor->id);
+
         $studentsInClassroom = $btsSioSlam->users()->where('role_id', $student->id)->get();
 
-        // Attribue des notes à ces étudiants pour l'évaluation
         foreach ($studentsInClassroom as $student) {
             Note::factory()
                 ->create([
-                    'value' => rand(0, 20), // Note aléatoire entre 0 et 20
+                    'value' => rand(0, 20),
                     'user_id' => $student->id,
                     'evaluation_id' => $evaluation->id,
                 ]);
